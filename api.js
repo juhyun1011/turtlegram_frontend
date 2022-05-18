cb677e254a6b8660a439c44951a1b3e6adc6548a
juhyun1011 <juhyun4567@gmail.com>
2022-05-18 19:36:25 +0900

    Feat : Log-in
    로그인 & 회원가입 api js 작성 및 메인 페이지로 연결

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
+const backend_base_url = "http://127.0.0.1:5000"
+const frontend_base_url = "http://127.0.0.1:5500"
+
 async function handleSignup() {
 
     const signupData = {
@@ -5,7 +8,7 @@ async function handleSignup() {
         password: document.getElementById("floatingPassword").value
     }
 
-    const response = await fetch('http://127.0.0.1:5000/signup', {
+    const response = await fetch(`${backend_base_url}/signup`, {
         method: 'POST',
         body: JSON.stringify(signupData)
     }
@@ -16,11 +19,54 @@ async function handleSignup() {
     // 회원가입 성공 시 로그인 페이지로 이동
     if (response.status == 201) {
         alert('회원가입 성공!')
-        window.location.replace("http://127.0.0.1:5500/login.html")
+        window.location.replace(`${frontend_base_url}/login.html`)
     }
     // 회원가입 실패 시
     else {
         alert('잘못된 입력입니다.')
+        window.location.reload()
+    }
+}
+
+
+async function handleLogin() {
+    console.log("handleLogin")
+
+    const loginData = {
+        email: document.getElementById("floatingInput").value,
+        password: document.getElementById("floatingPassword").value
     }
 
+    const response = await fetch(`${backend_base_url}/login`, {
+        method: 'POST',
+        body: JSON.stringify(loginData)
+    }
+    )
+
+    // if (response.status == 200) {
+    //     alert('')
+    //     window.location.replace(`${frontend_base_url}/index.html`);
+    // }
+
+    console.log(response)
+
+    response_json = await response.json()
+    console.log(response_json)
+    localStorage.setItem("token", response_json.token)
+
+}
+
+async function getName() {
+
+    const response = await fetch(`${backend_base_url}/getuserinfo`, {
+        headers: {
+            'Authorization': localStorage.getItem("token")
+        }
+    }
+    )
+    response_json = await response.json()
+    console.log(response_json)
+
+    const username = document.getElementById("username")
+    username.innerText = response_json.email
 }
